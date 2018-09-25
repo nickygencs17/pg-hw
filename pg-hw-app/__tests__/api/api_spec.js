e5741b94d01d90174c2cbe38ce50cfd7caa47ebd
CAMP\GENCN <Lacrosse3>
2018-09-25 16:31:08 -0400

    Test Cases

--- pg-hw-app/__tests__/api/api_spec.js
+++ pg-hw-app/__tests__/api/api_spec.js
@@ -1,6 +1,19 @@
 const frisby = require('frisby');
-//./node_modules/.bin/jest
-it('should be a teapot', function () {
-  return frisby.get('http://httpbin.org/status/418')
-    .expect('status', 418);
+
+it('it\'s all okay', function () {
+  return frisby.get('http://localhost')
+    .expect('status', 200);
+});
+
+it ('GET should return a status of 200 OK', function () {
+  return frisby
+    .get('http://localhost',{
+      title: 'My New Blog Post',
+      content: '<p>A cool blog post!</p>'
+    }).expect('status', 200);
+});
+
+it ('GET should return a status of 404 NOT FOUND', function () {
+  return frisby
+    .get('http://localhost/dosentwork').expect('status', 404);
 });
--- pg-hw-app/routes/routes.js
+++ pg-hw-app/routes/routes.js
@@ -3,6 +3,7 @@ var tacoController = require("./../controllers/tacoController.js");
 
 var appRouter = function (app) {
   app.get("/", function(req, res) {
+    
     price= tacoController.sayHelloInEnglish();
 
     res.status(200).send("Welcome to our restful API" + price);
